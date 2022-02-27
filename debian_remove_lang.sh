c459bb98e2207d4511c2397ac8d158d7969176c2
m-molinari <60198585+m-molinari@users.noreply.github.com>
2022-02-27 12:25:27 +0100

    Update debian_remove_lang.sh

--- debian_remove_lang.sh
+++ debian_remove_lang.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-/usr/bin/dpkg -l | grep firefox-esr-l10n | grep -v firefox-esr-l10n-it | grep -v firefox-esr-l10n-en-gb | awk '{print $2}' | xargs sudo apt-get remove -y
+echo "Removing Firefox languages excluding en and it"
+dpkg -l | grep firefox-esr-l10n | grep -v firefox-esr-l10n-it | grep -v firefox-esr-l10n-en-gb | awk '{print $2}' | xargs sudo apt-get remove --purge -y
+echo
+
+echo "Removing aspell dictionary excluding en and it"
+dpkg -l | grep aspell | grep dictionary | grep -v aspell-it | grep -v aspell-en | awk '{print $2}' | xargs sudo apt-get remove --purge -y
+
+echo "Removing hunspehunspell dictionary excluding en and it"
+dpkg -l | grep hunspell | grep dictionary | grep -v hunspell-it | grep -v hunspell-en- | awk '{print $2}' | xargs sudo apt-get remove --purge -y
