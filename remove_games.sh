03c92a9f8d0b982d48cd6080e34389ed5cfe989c
ser <mustang.caster@gmai.com>
2022-03-01 20:01:09 +0100

    script for removing games from debian cinnamon DE

---
+++ remove_games.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get remove -y --purge  gnome-chess gnome-klotski gnome-mahjongg gnome-mines gnome-nibbles gnome-robots gnome-sudoku \
+				 gnome-taquin gnome-tetravex hitori iagno lightsoff quadrapassel swell-foop five-or-more tali aisleriot
